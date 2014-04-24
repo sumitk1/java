bae21b3501feaead5dde854a6f1ad91807a57275
Sumit <sumitk.85@gmail.com>
2014-04-24 00:15:14 -0700

    Finishing the JUnit test with some basic validation

--- src/activeMind/MergeSort.java
+++ src/activeMind/MergeSort.java
@@ -2,36 +2,50 @@ package activeMind;
 
 public class MergeSort {
 
-	public static int[] merge(int[] arrayA, int[] arrayB) {
+	public int[] merge(int[] arrayA, int[] arrayB) {
 
-		// This is the new array which will contain all the elements from arrayA & arrayB
-	    int[] result = new int[arrayA.length + arrayB.length];  
+		 if (arrayA != null && arrayB != null){
+			 
+			// This is the new array which will contain all the elements from arrayA & arrayB
+		    int[] result = new int[arrayA.length + arrayB.length];  
+		    
+		    /*
+		     *  Counters
+		     *  i - arrayA
+		     *  j - arrayB
+		     *  k - resultArray 
+		     */
+		    int i = 0, j = 0, k = 0;
+		    
+		    while (i < arrayA.length && j < arrayB.length)
+		    {
+		    	// Loop through all the elements
+		    	if (arrayA[i] < arrayB[j])       
+		            result[k++] = arrayA[i++];
+		        else        
+		            result[k++] = arrayB[j++]; 
+		    }
+	
+		    // Since all the elements of arrayB is done comparing, put all the 
+		    // remaining elements of arrayA at the end of the result
+		    while (i < arrayA.length) {
+		    	result[k++] = arrayA[i++];
+		    }
+		    
+		    // Since all the elements of arrayA is done comparing, put all the 
+		    // remaining elements of arrayB at the end of the result
+		    while (j < arrayB.length) {
+		    	result[k++] = arrayB[j++];
+		    }
+		    
+		    return result;
+		    
+	    } else {
+	    	System.out.println("Arrays are empty");
+	    	int[] result = {-1};
+	    	return result;
+	    }
 	    
-	    /*
-	     *  Counters
-	     *  i - arrayA
-	     *  j - arrayB
-	     *  k - resultArray 
-	     */
-	    int i = 0, j = 0, k = 0;
-
-	    while (i < arrayA.length && j < arrayB.length)
-	    {
-	    	// Loop through all the elements
-	    	if (arrayA[i] < arrayB[j])       
-	            result[k++] = arrayA[i++];
-
-	        else        
-	            result[k++] = arrayB[j++]; 
-	    }
-
-	    while (i < arrayA.length) {
-	    	result[k++] = arrayA[i++];
-	    }
-	    while (j < arrayB.length) {
-	    	result[k++] = arrayB[j++];
-	    }
-	    return result;
 	}
 	
 	public static void main(String[] args) {
@@ -41,10 +55,11 @@ public class MergeSort {
 		int[] arrayA = {1,4,7,12,31,56,82,99};
 		int[] arrayB = {3,5,11,16,21,45,50,108};
 		
-		int [] result = MergeSort.merge(arrayA, arrayB);
+		MergeSort mergeSort = new MergeSort();
+		int [] result = mergeSort.merge(arrayA, arrayB);
 		
 		for(int i : result ) {
-			System.out.println(i);
+			System.out.print(i);
 		}		
 	}
 	
--- src/activeMind/MergeSortTest.java
+++ src/activeMind/MergeSortTest.java
@@ -14,19 +14,47 @@ import org.junit.Test;
  */
 public class MergeSortTest {
 
+	private MergeSort mergeSort = null;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		mergeSort = new MergeSort();
+		
 	}
 
 	/**
 	 * Test method for {@link activeMind.MergeSort#merge(int[], int[])}.
 	 */
 	@Test
-	public void testMerge() {
-		fail("Not yet implemented");
+	public void testNullArrays() {
+
+		int[] arrayA = null;
+		int[] arrayB = null;
+		int[] result = mergeSort.merge(arrayA, arrayB);
+		
+		assertTrue(result[0] == -1);
+		
 	}
+	
+	/**
+	 * Test method for {@link activeMind.MergeSort#merge(int[], int[])}.
+	 */
+	@Test
+	public void testMerge() {
+
+		int[] arrayA = {1,4,7,12,31,56,82,99};
+		int[] arrayB = {3,5,11,16,21,45,50,108};
+		int[] expectedResult = {1,3,4,5,7,11,12,16,21,31,45,50,56,82,99,108};
+		
+		int[] result = mergeSort.merge(arrayA, arrayB);
+		
+		assertArrayEquals(expectedResult, result);
+		
+	}
+	
+	
 
 }
