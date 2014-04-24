7847f79762619bce2c6798da4d5f8b4faa7c88ff
Sumit <sumitk.85@gmail.com>
2014-04-23 23:51:38 -0700

    Finishing the Merging of two arrays

--- src/activeMind/MergeSort.java
+++ src/activeMind/MergeSort.java
@@ -15,12 +15,37 @@ public class MergeSort {
 	     */
 	    int i = 0, j = 0, k = 0;
 
-	    while (i < arrayA.length && j < arrayA.length)
+	    while (i < arrayA.length && j < arrayB.length)
 	    {
 	    	// Loop through all the elements
+	    	if (arrayA[i] < arrayB[j])       
+	            result[k++] = arrayA[i++];
+
+	        else        
+	            result[k++] = arrayB[j++]; 
 	    }
 
+	    while (i < arrayA.length) {
+	    	result[k++] = arrayA[i++];
+	    }
+	    while (j < arrayB.length) {
+	    	result[k++] = arrayB[j++];
+	    }
 	    return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		//{1,4,7,12,31,56,82,99} and {3,5,11,16,21,45,50,108}  
+		
+		int[] arrayA = {1,4,7,12,31,56,82,99};
+		int[] arrayB = {3,5,11,16,21,45,50,108};
+		
+		int [] result = MergeSort.merge(arrayA, arrayB);
+		
+		for(int i : result ) {
+			System.out.println(i);
+		}		
 	}
 	
 }
