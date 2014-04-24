9572e52577b2e1358c66550117d0eab00e0009cb
Sumit <sumitk.85@gmail.com>
2014-04-23 23:52:58 -0700

    Adding JUnit test file for testing Merge Sort

---
+++ src/activeMind/MergeSortTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package activeMind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sumit
+ *
+ */
+public class MergeSortTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link activeMind.MergeSort#merge(int[], int[])}.
+	 */
+	@Test
+	public void testMerge() {
+		fail("Not yet implemented");
+	}
+
+}
