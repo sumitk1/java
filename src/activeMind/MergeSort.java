221d7119e5ec4fded8e23a5c503ccdf5ca698de7
Sumit <sumitk.85@gmail.com>
2014-04-23 23:29:50 -0700

    New result array to store the result

--- src/activeMind/MergeSort.java
+++ src/activeMind/MergeSort.java
@@ -2,4 +2,25 @@ package activeMind;
 
 public class MergeSort {
 
+	public static int[] merge(int[] arrayA, int[] arrayB) {
+
+		// This is the new array which will contain all the elements from arrayA & arrayB
+	    int[] result = new int[arrayA.length + arrayB.length];  
+	    
+	    /*
+	     *  Counters
+	     *  i - arrayA
+	     *  j - arrayB
+	     *  k - resultArray 
+	     */
+	    int i = 0, j = 0, k = 0;
+
+	    while (i < arrayA.length && j < arrayA.length)
+	    {
+	    	// Loop through all the elements
+	    }
+
+	    return result;
+	}
+	
 }
