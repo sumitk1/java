fb3d7569eadfe84337efd43d5b1515dbfec8b3f4
Sumit <sumitk.85@gmail.com>
2014-04-27 13:15:42 -0700

    Program to find sub array with max sum in a given Array

---
+++ src/dataStructures/Arrays.java
@@ -0,0 +1,38 @@
+package dataStructures;
+
+public class Arrays {
+	
+	public int maxSum (int[] arr) {
+		
+		int i = 0;
+		int maxSum = 0;
+		int currentSum = 0;
+		
+		for (i=0; i< arr.length; i++) {
+			
+			currentSum += arr[i];
+			
+			if (currentSum < 0) {
+				currentSum = 0;
+				
+			}
+			if (maxSum < currentSum) {
+				maxSum = currentSum;
+			}
+			
+		}
+		return maxSum;
+		
+	}
+		
+	public static void main(String[] args) {
+		
+		int[] input= {-1, -11, -14, -13, -5, -2};
+		
+		Arrays obj = new Arrays();
+		int ret = obj.maxSum(input);
+		System.out.println(ret);
+	}
+	
+
+}
