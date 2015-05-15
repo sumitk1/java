ae38f303d93b6d7a99226627a8fc52878af80e90
SumitKumar <sumitk.85@gmail.com>
2015-05-15 16:25:18 -0700

    Adding timeout

--- src/jerseyHttpClient/httpSyncClient.java
+++ src/jerseyHttpClient/httpSyncClient.java
@@ -3,14 +3,22 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
-
+import com.sun.jersey.api.client.Client;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
 public class httpSyncClient {
 
     public static void main(String [] args){
-        callEndpoint("hello", "payload");
+
+        /**
+         * This is how you run it
+         * $ javac -cp ../../lib/jersey-bundle-1.19.jar httpSyncClient.java
+         * $ java -cp .:../../lib/jersey-bundle-1.19.jar httpSyncClient
+         */
+
+        String payload = "{\"id\":837686871,\"funcname\":\"SeoV2\",\"args\":null,\"source\":null,\"product\":null}";
+        callEndpoint("hello", payload);
     }
 
     public static void callEndpoint(String endpoint, String payload) {
@@ -19,6 +27,10 @@ public class httpSyncClient {
 
             ClientConfig config = new DefaultClientConfig();
             Client client = Client.create(config);
+            client.setConnectTimeout(5000);
+            client.setReadTimeout(10000);
+
+            //Client client = ClientBuilder.newClient();
             WebResource webResource = client.resource(getBaseURI(endpoint));
 
             // POST method
@@ -40,7 +52,7 @@ public class httpSyncClient {
     }
 
     private static URI getBaseURI(String endpoint) {
-        String url = "http://localhost/sumitk.in/test.php";
+        String url = "http://sfo2-dev-vmdev008.sfo2.zoosk.com:9088/worker/v2/SeoV2";
         return UriBuilder.fromUri(url).build();
     }
 
